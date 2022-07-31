58df0a1e0d913cbae555f3e8a0e39c56cf6539e4
Dmytro <dmytroivanovichn@gmail.com>
2022-07-31 21:16:03 +0300

    slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+   $('#big_slider').slick(
+      {
+         dots: false,
+         infinite: true,
+         speed: 300,
+         slidesToShow: 1
+      }
+   );
+
+ });
+    
