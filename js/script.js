a3a11b3f0b879429c8f8e10e8ef1e9504f4045c0
Dmytro <dimores95@gmail.com>
2022-08-11 16:55:48 +0300

    finished work on the big slider

--- js/script.js
+++ js/script.js
@@ -9,5 +9,43 @@ $(document).ready(function(){
       }
    );
 
+   const pages = $("#big_slider").slick("getSlick").slideCount;
+   $(".number_pages").text(pages);
+
+   $('.s-next').on('click', ()=> {
+      $(".slick-next").click();
+      $(".current_page").text(getCurrentPage());
+   });
+
+   $('.s-prev').on('click', ()=> {
+      $(".slick-prev").click();
+      $(".current_page").text(getCurrentPage());
+   });
+
+   $('#big_slider').on('swipe', () => {
+      $(".current_page").text(getCurrentPage());
+    });
+
+   const getCurrentPage = () => {
+      const index = document.querySelector('#big_slider .slick-active')
+      .getAttribute('data-slick-index');
+
+      let currentPage  = parseInt(index) + 1;
+
+      if(currentPage >= 10) {
+         return currentPage;
+      }
+      else {
+         return "0" + currentPage;
+      }
+   }
+
+   $('#smoll_slider').slick({
+      infinite: true,
+      dots: false,
+      slidesToShow: 3,
+      slidesToScroll: 3 
+   });
+
  });
     
